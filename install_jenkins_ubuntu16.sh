7138012f78f655a6a9887bd5b4aa07431c7e8ecc
Abdoul Aw <unixsvr4@gmail.com>
2018-02-07 11:51:35 -0500

    5th commit

--- install_jenkins_ubuntu16.sh
+++ install_jenkins_ubuntu16.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+###echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+echo deb https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt update
 sudo apt install jenkins
+sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 sudo ufw allow 8080
---
+++ run_jenkins_manually2.sh
@@ -0,0 +1,2 @@
+### run as jenkins
+/usr/bin/java -Djava.awt.headless=true -jar /usr/share/jenkins/jenkins.war --webroot=/var/cache/jenkins/war --httpPort=8085
