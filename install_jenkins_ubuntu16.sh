0aa56d754073a7f6fd1a7ae74ae696627d9ed57a
Abdoul Aw <unixsvr4@gmail.com>
2018-02-07 10:31:03 -0500

    3rd commit

--- install_jenkins_ubuntu16.sh
+++ install_jenkins_ubuntu16.sh
@@ -3,3 +3,9 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt update
 sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo ufw allow 8080
+sudo ufw status
+sudo ufw allow OpenSSH
+sudo ufw enable
